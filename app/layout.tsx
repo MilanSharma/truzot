81fd153a187639aaf9ae35c7c11e6d39db86a08f
Milan Sharma <milansharma82@gmail.com>
2026-07-12 22:13:35 -0700

    Add Facebook Meta Pixel (1385221020153603) for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -126,6 +126,33 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             `,
           }}
         />
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1385221020153603');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1385221020153603&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <link rel="dns-prefetch" href="https://fal.ai" />
         <link rel="preconnect" href="https://fal.ai" />
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
